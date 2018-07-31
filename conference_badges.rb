518f42aad8efe94c787c4e0386c231f38cd6b432
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-07-31 16:45:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,10 @@
 # Write your code here.
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator[attendees]
+def batch_badge_creator (names)
+  
 end
