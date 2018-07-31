28244184946b41ac4b238c9789529e9aebdbcecb
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-07-31 16:34:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker (name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator[names]
+  badge_array []
+  names.each |name| do
+    badge_array.push "Hello, my name is #{name}."
+  end
+  badge_array
+end
