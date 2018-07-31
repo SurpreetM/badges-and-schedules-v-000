7604b94fed33d9af4e783d8119c7787a4e3cb6dd
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-07-31 16:41:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,15 @@
 # Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def new_method [attendees]
+def batch_badge_creator [names]
+  badges_array = []
+
+  names.each |name| do
+    badges_array.push "Hello, my name is #{name}."
+  end
+  badges_array
 end
